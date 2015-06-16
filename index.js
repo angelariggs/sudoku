c2890805eff9d1686ed162614da0e58183fa1bd1
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-16 11:28:50 -0700

    grid displays with spacing

--- index.js
+++ index.js
@@ -14,18 +14,27 @@ var replaceString = numberString.replace(delimiter, ' ');
 // console.log("|" + replaceString.substr(18,3) + "|" + replaceString.substr(21,3) + "|" + replaceString.substr(24,3) + "|\n");
 
 
+// function displayGrid () {
+//   for (var i = 0; i < replaceString.length; i +=9) {
+//     console.log("+---------+---------+---------+");
+//     console.log("|" + replaceString.substr(i, 3) + "|" + replaceString.substr(i+3, 3) + "|" + replaceString.substr(i+6, 3) + "|\n");
+//   }
+//   console.log("+---------+---------+---------+");
+// }
+
+// displayGrid();
+
 function displayGrid () {
-  for (var i = 0; i < replaceString.length; i +=9) {
-    console.log("+---------+---------+---------+");
-    console.log("|" + replaceString.substr(i, 3) + "|" + replaceString.substr(i+3, 3) + "|" + replaceString.substr(i+6, 3) + "|\n");
+  for (var i = 0; i<replaceString.length; i+=9) {
+    if (i%27 === 0) {
+      console.log("+---------+---------+---------+");
+    }
+    
+    console.log("| " + replaceString[i] + "  " + replaceString[i+1] + "  " + replaceString[i+2] + " | "
+    + replaceString[i+3] + "  " + replaceString[i+4] + "  " + replaceString[i+5] + " | "
+    + replaceString[i+6] + "  " + replaceString[i+7] + "  " + replaceString[i+8] + " |")
   }
-  console.log("+---------+---------+---------+");
+  console.log("+---------+---------+---------+")
 }
 
 displayGrid();
-
-function displayGrid () {
-  for (var i = 0; i<replaceString.length; i++) {
-    console.log("| " + replaceString[i] + "  " + replaceString[i+1] + "  " + replaceString[2] + " ")
-  }
-}
