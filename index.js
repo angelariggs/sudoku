b59b035ab14465ac61e33cae3f1adf27e403f056
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-16 11:15:43 -0700

    created function to display game board; need to fix spacing with diff. function

--- index.js
+++ index.js
@@ -4,4 +4,28 @@ var delimiter = /[.,_]/g;
 
 var replaceString = numberString.replace(delimiter, ' ');
 
-document.write('"%s"', replaceString);
+// console.log('"%s"', replaceString);
+//document.write(replaceString);
+
+// console.log("|" + replaceString.substr(0,3) + "|" + replaceString.substr(3,3) + "|" + replaceString.substr(6,3) + "|\n");
+
+// console.log("|" + replaceString.substr(9,3) + "|" + replaceString.substr(12,3) + "|" + replaceString.substr(15,3) + "|\n");
+
+// console.log("|" + replaceString.substr(18,3) + "|" + replaceString.substr(21,3) + "|" + replaceString.substr(24,3) + "|\n");
+
+
+function displayGrid () {
+  for (var i = 0; i < replaceString.length; i +=9) {
+    console.log("+---------+---------+---------+");
+    console.log("|" + replaceString.substr(i, 3) + "|" + replaceString.substr(i+3, 3) + "|" + replaceString.substr(i+6, 3) + "|\n");
+  }
+  console.log("+---------+---------+---------+");
+}
+
+displayGrid();
+
+function displayGrid () {
+  for (var i = 0; i<replaceString.length; i++) {
+    console.log("| " + replaceString[i] + "  " + replaceString[i+1] + "  " + replaceString[2] + " ")
+  }
+}
