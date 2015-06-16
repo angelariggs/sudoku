2fa9fba10853f3707834d478c19796f9c759c005
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-16 10:29:55 -0700

    tested delimiter replacing and console log string of numbers

---
+++ index.js
@@ -0,0 +1,7 @@
+var numberString = '158.2..6.2...8..9..3..7.8.2.6.74......4.6.7......19.5.4.9.3..2..2..5...8.7..9.413'
+
+var delimiter = /[.,_]/g;
+
+var replaceString = numberString.replace(delimiter, ' ');
+
+document.write('"%s"', replaceString);
